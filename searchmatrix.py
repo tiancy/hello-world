9a4f7df49be80d1be2005be06721184653ea525c
cy <tianchunyu@outlook.com>
2016-08-06 16:48:19 +0800

    new file: searchmatrix.py

---
+++ searchmatrix.py
@@ -0,0 +1,40 @@
+# Source https://leetcode.com/problems/search-a-2d-matrix-ii/
+# Author cytian
+# Updated 2016-08-06
+
+# Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+#
+# Integers in each row are sorted in ascending from left to right.
+# Integers in each column are sorted in ascending from top to bottom.
+# For example,
+#
+# Consider the following matrix:
+#
+# [
+#   [1,   4,  7, 11, 15],
+#   [2,   5,  8, 12, 19],
+#   [3,   6,  9, 16, 22],
+#   [10, 13, 14, 17, 24],
+#   [18, 21, 23, 26, 30]
+# ]
+# Given target = 5, return true.
+#
+# Given target = 20, return false.
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        row = 0
+        column = len(matrix[0]) - 1
+        while row < len(matrix) and column >= 0:
+            if matrix[row][column] == target:
+                return True
+            elif matrix[row][column] > target:
+                column = column - 1
+            else:
+                row = row + 1
+        return False
