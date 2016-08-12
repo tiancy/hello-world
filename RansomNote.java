45dd58926b489155289ed4d2befa5e6caff5bd7c
cy <tianchunyu@outlook.com>
2016-08-12 15:32:41 +0800

    new file RansomNote.java

---
+++ RansomNote.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Source  https://leetcode.com/problems/find-peak-element/
+ * Author  cytian
+ * Created 2016-08-12
+ *
+ * Given  an  arbitrary  ransom  note  string  and  another  string  containing  letters from  all  the  magazines, 
+ * write  a  function  that  will  return  true  if  the  ransom   note  can  be  constructed  from  the  magazines ;
+ * otherwise,  it  will  return  false.   
+ *
+ * Each  letter  in  the  magazine  string  can  only  be  used  once  in  your  ransom  note.
+ *
+ * Note:
+ * You may assume that both strings contain only lowercase letters.
+ *
+ * canConstruct("a", "b") -> false
+ * canConstruct("aa", "ab") -> false
+ * canConstruct("aa", "aab") -> true
+ */
+public class RansomNote {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char[] notes = ransomNote.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        int index = -1;
+        for (int i = 0; i < notes.length; i++) {
+            if (map.containsKey(notes[i]) && (index = magazine.indexOf(notes[i], map.get(notes[i]) + 1)) < 0)
+                return false;
+            else if (!map.containsKey(notes[i]) && (index = magazine.indexOf(notes[i])) < 0)
+                return false;
+            map.put(notes[i], index);
+        }
+        return true;
+    }
+}
