44ac3f5a8ecc9df834671e67eb0b98a9f5a6390c
Tian <tianchunyu@outlook.com>
2018-11-19 19:24:14 +0800

    Update and rename findDuplicate.go to findduplicate.go

--- findduplicate.go
+++ findduplicate.go
@@ -1,3 +1,25 @@
+/*
+Source  https://leetcode.com/problems/find-the-duplicate-number/
+Author  Tian
+Date    2018/11/17
+
+287. Find the Duplicate Number
+
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive),
+prove that at least one duplicate number must exist. Assume that there is only one duplicate number,
+find the duplicate one.
+
+Example 1:
+
+	Input: [1,3,4,2,2]
+	Output: 2
+
+Example 2:
+
+	Input: [3,1,3,4,2]
+	Output: 3
+
+*/
 package main
 
 import (
